3ededf6c4c24f17575254e0ca1a512ef76cad1d8
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-09 09:55:29 +0900

    [Lv.0] 덧셈식 출력하기

---
+++ programmers/coding-basics-training/day2-출력, 연산/덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+<<<<<<< HEAD
+    String a = sc.next();
+    String b = sc.next();
+    System.out.println(a.concat(b));
+=======
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + " + " + b + " = " + (a + b));
+>>>>>>> 3219683 ([Lv.0] 덧셈식 출력하기)
+  }
+}
