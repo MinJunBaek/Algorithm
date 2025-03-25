699e2101cf6acea4d2a4d6544b961647ccbee974
MinJunBaek <baekminjun92@gmail.com>
2025-03-25 22:26:25 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.89 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+  public String solution(String my_string, int[][] queries) {
+    StringBuilder answer = new StringBuilder(my_string);
+    for (int[] query : queries) {
+      int s = query[0];
+      int e = query[1];
+      String reversed = new StringBuilder(answer.substring(s,  e + 1)).reverse().toString();
+      answer.replace(s, e + 1, reversed);
+    }
+    return answer.toString();
+  }
+}
