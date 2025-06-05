09d2d610990446cbd2dd90e905a6132a9e723603
MinJunBaek <baekminjun92@gmail.com>
2025-06-05 11:16:58 +0900

    [level 0] Title: 문자열 출력하기, Time: 213.35 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
