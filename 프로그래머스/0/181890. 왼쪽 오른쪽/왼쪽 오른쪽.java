d628fed7589dd47baa17768e192744e6202f5750
MinJunBaek <baekminjun92@gmail.com>
2025-06-30 06:31:42 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,13 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] str_list) {
+        String[] answer = {};
         for (int i = 0; i < str_list.length; i++) {
             if (str_list[i].equals("l")) {
-                return Arrays.copyOfRange(str_list, 0, i);
+                answer = Arrays.copyOfRange(str_list, 0, i);
+                break;
             } else if (str_list[i].equals("r")) {
-                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+                answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
+                break;
             }
         }
-        return new String[0];
+        return answer;
     }
 }
