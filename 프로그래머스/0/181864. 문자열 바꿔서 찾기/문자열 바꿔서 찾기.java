467ea76a00212343aa65791e90be58f4670809ec
MinJunBaek <baekminjun92@gmail.com>
2025-07-29 10:24:54 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String str = "";
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.charAt(i) == 'A') {
-                str += "B";
-            } else {
-                str += "A";
-            }
-        }
+        String str = myString.replace("A", "a").replace("B", "A").replace("a", "B");
         int answer = str.contains(pat) ? 1 : 0;
         return answer;
     }
