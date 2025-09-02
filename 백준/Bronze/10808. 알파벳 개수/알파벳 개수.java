e02d75c012630014fb61b26aff077a64bf1896a6
MinJun Baek <baekminjun92@gmail.com>
2025-09-02 13:21:10 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 188 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] arr = new int['z' - 'a' + 1];
+        
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            arr[idx] += 1;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
