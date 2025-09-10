9d8f385a09be4a8d30db1f43b5fc3af1797db95b
MinJun Baek <baekminjun92@gmail.com>
2025-09-10 21:11:26 +0900

    [Bronze II] Title: 방 배정, Time: 112 ms, Memory: 14520 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[2][6];
+        int rooms = 0;
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[s][y]++;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > 0) {
+                    rooms += (arr[i][j] + k - 1) / k;
+                }
+            }
+        }
+        
+        System.out.println(rooms);
+    }
+}
